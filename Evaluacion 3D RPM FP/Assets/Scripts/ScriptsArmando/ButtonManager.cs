f2463aa3fff704d125a30a4d7493d99f25993ae4
victoria-140907 <victoria.solis.prieto7@gmail.com>
2026-04-29 02:48:24 +0200

    escena titulo te lleva a juego

--- Evaluacion 3D RPM FP/Assets/Scripts/ScriptsArmando/ButtonManager.cs
+++ Evaluacion 3D RPM FP/Assets/Scripts/ScriptsArmando/ButtonManager.cs
@@ -5,7 +5,7 @@ public class ButtonManager : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(2);
     }
 
     public void QuitGame()
